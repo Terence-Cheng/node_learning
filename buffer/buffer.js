00df3fafd0d452372f67ed36ad7a8b930d7b0d49
buyixiaojiang <chengtingsen@163.com>
2016-10-05 22:46:44 +0800

    buffer

---
+++ buffer/buffer.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+fs.readFile('logo.png', function (err, origin_buffer) {
+	console.log(Buffer.isBuffer(origin_buffer));
+
+	fs.writeFile('logo_buffer.png', origin_buffer, function(err) {
+		if (err) {
+			console.log('err: %s', err);
+		}
+	});
+
+	var base64Image = origin_buffer.toString('base64');
+
+	var decodeImage = new Buffer(base64Image, 'base64');
+
+	console.log(Buffer.compare(origin_buffer, decodeImage));
+
+	fs.writeFile('logo_decoded.png', decodeImage, function(err) {
+		if (err) {
+			console.log('err: %s', err);
+		}
+	})
+})
