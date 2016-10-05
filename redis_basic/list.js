a4942ca295d8362d8e5fc29654bc0daf1d394220
buyixiaojiang <chengtingsen@163.com>
2016-10-05 10:46:24 +0800

    modify notes

--- redis_basic/list.js
+++ redis_basic/list.js
@@ -1,11 +1,12 @@
 var client = require('./client.js');
 
-/*client.rpush('testLists', 'a');  // similar to Array.prototype.push()
+client.rpush('testLists', 'a');  // similar to Array.prototype.push()
 client.rpush('testLists', 'b');
 client.rpush('testLists', 'c');
 client.rpush('testLists', '1');
-client.lpush('testLists', '2'); */  // similar to Array.prototype.unshift()
+client.lpush('testLists', '2');   // similar to Array.prototype.unshift()
 
+// similar to Array.prototype.shift(),
 client.lpop('testLists', function(err, v) {
 	console.log('client.lpop:', v);
 })
